2a9c221e43ec12a4ddad583215392479fb8d3e6b
Vamsi <vamsikrishna3296@gmail.com>
2021-07-20 00:26:45 +0530

    Playing with bits

---
+++ bitcount.c
@@ -0,0 +1,27 @@
+    //bitcount
+
+#include<stdio.h>
+
+    // bitcount: count 1 bits in x
+int bitcount(unsigned x)
+{
+  int b;
+
+  for (b = 0; x != 0; x >>= 1)
+  {
+    printf("Value of x is %d\n", x);
+    if ( x & 01 )
+    {
+      b++;
+      printf("The value of b is %d\n", b);
+    }
+  }
+  return b;
+}
+
+int main()
+{
+  int k;
+  k = bitcount(11);
+  printf("Returned value is %d\n", k);
+}
